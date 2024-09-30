9f768fa287f8a4e26695c26e7720f3b5a3db2b7e
Sean Folkard <sean_folkard@dlsu.edu.ph>
2024-09-30 12:44:08 +0800

    loads all the images

--- ioc-image.py
+++ ioc-image.py
@@ -1,15 +1,29 @@
+# Folkard, Sean Michael - OpenCV Homework No. 1
+
 import os
 import cv2
 
-# read image
-image_path = os.path.join("pics/new-jeans.jpg")
+# Load the image
+image_path = os.path.join('pics/hanni-new-jeans.jpg')
 img = cv2.imread(image_path)
 
-# write image (save a copy)
-#cv2.imwrite(os.path.join('.', 'pics', 'shibainu1_out.jpg'), img)
+new_width = int(img.shape[1] * 0.5)
+new_height = int(img.shape[0] * 0.5)
+new_dim = (new_width, new_height)
 
-# visualize/display image 
-cv2.imshow("Original", img)
+resized_img = cv2.resize(img, new_dim, interpolation=cv2.INTER_AREA)
+
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cropped_img_rgb = img_rgb[120:450, 320:700]
+cropped_img_hsv = img_hsv[120:450, 320:700]
+
+print(img.shape)
+print(resized_img.shape)
+
+cv2.imshow('Original', img)
+cv2.imshow('Scaled by 50%', resized_img)
+cv2.imshow("Cropped RGB", cropped_img_rgb)
+cv2.imshow("Cropped HSV", cropped_img_hsv)
+
 cv2.waitKey(0)
-
-
