c3d2d467a57319053f89d963adc7b3bd0a95cce4
Sean Folkard <sean_folkard@dlsu.edu.ph>
2024-09-30 11:19:53 +0800

    window appears for original and scaled by 50%

--- ioc-image.py
+++ ioc-image.py
@@ -9,7 +9,7 @@ img = cv2.imread(image_path)
 #cv2.imwrite(os.path.join('.', 'pics', 'shibainu1_out.jpg'), img)
 
 # visualize/display image 
-cv2.imshow('image', img)
+cv2.imshow('Original', img)
 cv2.waitKey(0)
 
 
--- op-resize.py
+++ op-resize.py
@@ -2,14 +2,20 @@
 import os
 import cv2
 
-image_path = os.path.join("pics\smiski.jpg")
+# Load the image
+image_path = os.path.join("pics", "smiski.jpg")
 img = cv2.imread(image_path)
 
-resized_img = cv2.resize(img,(640,640))
+new_width = int(img.shape[1] * 0.5)
+new_height = int(img.shape[0] * 0.5)
+new_dim = (new_width, new_height)
+
+resized_img = cv2.resize(img, new_dim, interpolation=cv2.INTER_AREA)
 
 print(img.shape)
 print(resized_img.shape)
 
-cv2.imshow('img', img)
-cv2.imshow('resized_img', resized_img)
+cv2.imshow('Original', img)
+cv2.imshow('Scaled by 50%', resized_img)
+
 cv2.waitKey(0)
